234ad54dcb8e00b52d21f6e5b6c21de33ba4af80
Akshay Kapoor <akshkap@amazon.com>
2023-08-10 14:26:31 +0200

    Added cloud9 installer

--- toolbox/installer.sh
+++ toolbox/installer.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Success"
+pushd /home/ec2-user/environment
+git clone https://akskap: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@github.com/akskap/AWS-DevOps-Simplified.git
+rm -rf .git
+
