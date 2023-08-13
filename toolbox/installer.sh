9c93bc24bcf7a6158b8c196fb5aa7f8515be6cdf
Akshay Kapoor <akshkap@amazon.com>
2023-08-13 15:21:02 +0200

    install packer in cloud9

--- toolbox/installer.sh
+++ toolbox/installer.sh
@@ -12,3 +12,10 @@ git remote add origin https://github.com/akskap/AWS-DevOps-Simplified.git
 git fetch origin
 git checkout main
 rm -rf .git
+
+# Install Tools
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install packer
+echo "export PATH=/usr/bin/packer:$PATH" >> /home/ec2-user/.bashrc
+echo "export PATH=/usr/bin/packer:$PATH" >> /home/ec2-user/.bash_profile
